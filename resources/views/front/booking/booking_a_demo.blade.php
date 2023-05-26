44be59885faa538a62f727cee85505c1495aaa9c
Ramesh Singh <rameshsingh07773@gmail.com>
2023-05-26 12:46:41 +0530

    dev css and book a demo form

--- resources/views/front/booking/booking_a_demo.blade.php
+++ resources/views/front/booking/booking_a_demo.blade.php
@@ -1,72 +1,5 @@
 @extends('front.layouts.master')
 @section('content')
-<<<<<<< HEAD
-<!-- contact page start -->
-<section class="contact-details">
-    <div class="form-left">
-    <h3>Contact Form</h3>
-
-    <div class="container">
-        <div class="columnsWrap sc_columns sc_columns_count_3 margin_top_middle margin_bottom_middle">
-            <div class=" sc_column_item sc_column_item_1 odd first span_2">
-                <div class="sc_contact_form sc_contact_form_contact contact_form_1">
-                    <h2 class="title">Address</h2>
-                    <form class="contact_1" method="post" action="include/contact-form.php">
-                        
-                    <div class="row mt-4">
-                             <div class="col-75">
-                        <div class="columnsWrap">
-                            
-                            <div class="columns1_3">
-                                <label class="required" for="contact_form_username">Country</label>
-                                <input type="text" name="name" id="contact_form_username">
-                            </div>
-                            <div class="columns1_3">
-                                <label class="required" for="contact_form_username">TimeZone</label>
-                                <input type="text" name="name" id="contact_form_username">
-                            </div>
-                           
-                        </div>
-                        </div>
-                        </div>
-
-                        <div class="result sc_infobox"></div>
-                    </form>
-                </div>
-                <div class="sc_contact_form sc_contact_form_contact contact_form_1">
-                    <h2 class="title">Booking Info</h2>
-                    <form class="contact_1" method="post" action="include/contact-form.php">
-                        
-                    <div class="row mt-4">
-                             <div class="col-75">
-                        <div class="columnsWrap">
-                            
-                            <div class="columns1_3">
-                                <label class="required" for="contact_form_username">Date Of Demo</label>
-                                <input type="text" name="name" id="contact_form_username">
-                            </div>
-                            <div class="columns1_3">
-                                <label class="required" for="contact_form_username">Time Of Damo</label>
-                                <input type="text" name="name" id="contact_form_username">
-                            </div>
-                           
-                        </div>
-                        </div>
-                        </div>
-
-                        <div class="sc_contact_form_button">
-                            <div class="squareButton sc_button_size sc_button_style_global global ico">
-                                <button type="submit" name="contact_submit"
-                                    class="contact_form_submit icon-comment-1">Send Message</button>
-                            </div>
-                        </div>
-
-                        <div class="result sc_infobox"></div>
-                    </form>
-                </div>
-            </div>
-
-=======
 <section>
     <div class="container">
         <div class="form-container sc_columns">
@@ -157,7 +90,6 @@
             </div>
         </div>
     </div>
->>>>>>> bdde0f8c261087d09b226084beaabc8c95102010
 </section>
 @endsection
 @section('customscript')
