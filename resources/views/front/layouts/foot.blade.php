2f662d2b18b9aa3eb2ad3051b81aef491dfc4786
prabhanshuxarma <prabhanshuxarma@gmail.com>
2023-06-22 22:07:45 +0530

    Slider Done

--- resources/views/front/layouts/foot.blade.php
+++ resources/views/front/layouts/foot.blade.php
@@ -1,5 +1,6 @@
 <script> var ajaxUrl = "{{url('/')}}"; </script>
 <script type='text/javascript' src='{{asset("assets/front/js/vendor/jquery-1.11.3.min.js")}}'></script>
+<script type='text/javascript' src='{{asset("assets/front/js/owl.carousel.min.js")}}'></script>
 <script type='text/javascript' src='{{asset("assets/front/js/vendor/jquery-migrate.min.js")}}'></script>
 <script type='text/javascript' src='{{asset("assets/front/js/vendor/revslider/rs-plugin/js/jquery.themepunch.tools.min.js")}}'></script>
 <script type='text/javascript' src='{{asset("assets/front/js/vendor/revslider/rs-plugin/js/jquery.themepunch.revolution.min.js")}}'></script>
@@ -20,4 +21,38 @@
 
 <script type='text/javascript' src="{{asset('/custom/assets/js/toastr.min.js')}}"></script>
 <script type='text/javascript' src="{{asset('/custom/assets/js/sweetalert-dev.js')}}"></script>
+
+
+ 
+<script>
+  $('.hero-slider').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:0,
+    items:1,
+    nav:true
+});
+
+$('.testimonials').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:20,
+    items:3,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+</script>
+
+
 @yield('customscript')
